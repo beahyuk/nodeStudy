e4ceab368f53ba6a53e38b1a9079799c7416ec6c
unknown <xueqing@rst.ricoh.com>
2020-07-10 11:01:12 +0800

    public

--- code/express-demo/app.js
+++ code/express-demo/app.js
@@ -11,9 +11,18 @@ app.get('/', function(req, res) {
 app.get('/post', function(req, res) {
     res.send("i'm post")
 });
-// 公开指定目录
-// 只要这样做了，你就可以直接通过 /public/xx 的方式访问public目录中的所有资源了
+
+/**
+ *  只要这样做了，你就可以直接通过 /public/xx 的方式访问public目录中的所有资源了
+ *  当以/public/ 开头的时候，去 ./public/目录中找到对应的资源
+ * 浏览器输入路径：http://127.0.0.1:3000/public/js/main.js  就可以访问到
+ */
+
 app.use('/public/', express.static('./public/'));
+
+// 当省略第一个参数时，可以通过省略/public 的方式去访问
+// 浏览器输入路径：http://127.0.0.1:3000/js/main.js  就可以访问到
+app.use(express.static('./public'))
 
 app.listen(3000, function() {
     console.log('服务器启动啦')
