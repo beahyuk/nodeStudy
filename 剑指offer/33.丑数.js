4e895ab6fa42f62cce4a196c3130addc47b97bb5
beahyuk <xueqing-950822@163.com>
2020-08-07 21:16:52 +0800

    update

---
+++ 剑指offer/33.丑数.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+    let res = [];
+    let p2 = 0,
+        p3 = 0,
+        p5 = 0;
+    res[0] = 1;
+    for (let i = 1; i < n; i++) {
+        res[i] = Math.min(res[p2] * 2, res[p3] * 3, res[p5] * 5);
+        if (res[i] === res[p2] * 2) p2++;
+        if (res[i] === res[p3] * 3) p3++;
+        if (res[i] === res[p5] * 5) p5++;
+
+    }
+    return res.pop()
+};
